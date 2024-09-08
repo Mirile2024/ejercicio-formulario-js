4c8105a695b74e2d70db6345261391f0b3383335
Mirile2024 <mirileas@gmail.com>
2024-09-08 19:53:27 -0300

    ejercicio 5 funcionando bien

--- scripts/index.js
+++ scripts/index.js
@@ -21,21 +21,24 @@ registerBtn.addEventListener("click", () => {
   console.log(`Acepta los terminos: ${terminos.value}`)
 
   // 5 Validamos si el usuario es mayor de edad. Si no, mostramos
-  let mayorEdad = document.querySelector(`[name=legalAge]:checked`)
-  if(mayorEdad = true){
-    console.log("Es Mayor de Edad");
-  }else{
-    console.log("Debes aceptar los TyCs para registrarte en el sitio");
-  }
-
-  // console.log(`{email: ${nombre}, password: ${contraseña},Mayor de edad: ${mayorEdad.value}}`)
-
-  
-  console.log(`{email: ${nombre}, password: ${contraseña},Mayor de edad: ${mayorEdad.value}}`)
-
   // un mensaje de error: "Debes ser mayor de edad para registrarte en el sitio"
+
+  const mayorEdad = document.querySelector(`[name=legalAge]:checked`)
+      if(mayorEdad==true){
+      console.log("Es Mayor de Edad")
+    }else{
+        console.log("Debes ser mayor de edad para registrarte en el sitio");
+      }
+    
+
   // 6 Vaidamos si el usuario aceptó los términos y condiciones. Si no, mostramos
   // un mensaje de error: "Debes aceptar los TyCs para registrarte en el sitio"
+  // const terminos = document.querySelector('[name=termsAndConditions]:checked')
+  // if(terminos = false){
+  //   console.log("Debes aceptar los TyCs para registrarte en el sitio");
+  // }
+  // console.log(`Acepta los terminos: ${terminos.value}`)
+
   // 7 Si todo esta correcto, mostramos por consola un objeto con la información
   // ingresada por el usuario.
 });
